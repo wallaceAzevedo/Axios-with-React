544ce6125ffdbf0cae9ef2c18ed7f517fac0230e
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-09-27 12:02:53 -0300

    criar um post com axios

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,16 +5,55 @@ import './App.css';
 function App() {
 
   const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+  const baseURLCREATEPOST = "https://jsonplaceholder.typicode.com/posts";
 
   const [post, setPost] = useState(undefined);
-  const [methods, seMethods] = useState (false);
-  const [visivel, setVisivel] = useState<"first" | "FEED" >("first");
+  const [visivel, setVisivel] = useState<"first" | "create post" | "">("");
+  const [createPosts, setCreatePosts] = useState(undefined);
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const handleChangeTitle = (event) => setTitle(event.target.value)
+  const handleChangeBody = (event) => setBody(event.target.value)
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
+    const getData = async () => {
+      try {
+        await axios.get(baseURL).then((response) => {
+          setPost(response.data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
   }, []);
+
+  // Create a Post
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        await axios.get(`${baseURLCREATEPOST}/1`).then((response) => {
+          setCreatePosts(response.data);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURLCREATEPOST, {
+        title: title,
+        body: body
+      })
+      .then((response) => {
+        setCreatePosts(response.data);
+      });
+  }
 
   const wall = () => {
     if (visivel === "first") {
@@ -25,12 +64,32 @@ function App() {
         </div>
       )
     }
+    if (visivel === "create post") {
+      return (
+        <div>
+          <input
+            value={title}
+            onChange={handleChangeTitle}
+            placeholder='Títle'
+          />
+          <input
+            value={body}
+            onChange={handleChangeBody}
+            placeholder='Body'
+          />
+          <button onClick={createPost}>Create Post</button>
+          <p>{createPosts.title}</p>
+          <p>{createPosts.body}</p>
+        </div>
+      )
+    }
   }
 
-  if (!post) return null;
+
   return (
     <div className="App">
       <button onClick={() => setVisivel('first')} >PRIMEIRO POST</button>
+      <button onClick={() => setVisivel('create post')} >Criar um POST</button>
       {wall()}
     </div>
   );
